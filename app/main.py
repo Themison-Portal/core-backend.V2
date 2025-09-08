ad272d909a2fcec3c9c53f4db5defea444841d92
FelixManFongCheung <manfongcheung74@gmail.com>
2025-09-08 15:48:17 +0200

    added multiple cors

--- app/main.py
+++ app/main.py
@@ -22,6 +22,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[os.getenv("FRONTEND_URL") or "http://localhost:3000"],  # Your frontend URL
+    "http://localhost:3000", 
     allow_methods=["*"],
     allow_headers=["*"],
     allow_credentials=True,
