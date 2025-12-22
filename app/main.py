a75b00a267d26dbda8d4d5cc2e46c6baf00930ce
Iftikhar-themison <iftikhar@themison.com>
2025-12-22 15:20:56 +0100

    updated redis url

--- app/main.py
+++ app/main.py
@@ -36,7 +36,7 @@ async def lifespan(app: FastAPI):
         try:
             redis_url = os.getenv("REDIS_URL")
             # redis_client = Redis(host="localhost", port=6379, db=0, decode_responses=False)
-            redis_client = redis.from_url(redis_url, decode_responses=True)
+            redis_client = Redis.from_url(redis_url, decode_responses=True)
             await redis_client.ping()
             app.state.redis_client = redis_client
             logging.info("Redis connection successful.")
